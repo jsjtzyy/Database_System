832695bb9dfaac623617caa039cd7725198f0201
yiy4 <yiy4@illinois.edu>
2017-04-18 15:53:44 -0500

    add link to movie on homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,7 @@
         <div class="col-md-4">
           <h2>Movie</h2>
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="dashboard/movie" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-default" href="movie" role="button">View details &raquo;</a></p>
        </div>
         <div class="col-md-4">
           <h2>Ride</h2>
